ec2e33dde1bfa39f35363640a7bf1951ab6bae25
Nikita Pushkarev <nikita.pushkarev@defa.ru>
2019-07-30 14:01:37 +0300

    Improve theme

--- components/common/MainMenu.vue
+++ components/common/MainMenu.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   menu.menu--main.bgc-light(:class='{"menu--minimize": minimizeMenu}')
-    ul 
-      NuxtLink.menu__link(v-for='(item, i) in mainMenu' :key='i' :to='item.link') 
+    ul
+      NuxtLink.menu__link(v-for='(item, i) in mainMenu' :key='i' :to='item.link')
         .menu__icon-box
-          AppIcon.menu__icon.icon-3.icon-darkest(:icon='item.icon')
+          AppIcon.menu__icon.icon-3.icon-darker(:icon='item.icon')
         h4.color-dark-link {{item.name}}
     .menu__footer
       AppToggle(:icons='["sun", "moon"]'
@@ -20,15 +20,10 @@ import { mapState, mapActions } from 'vuex'
 export default {
   name: 'MenuMenu',
   computed: {
-    ...mapState('page', [
-      'minimizeMenu',
-      'mainMenu',
-    ]),
+    ...mapState('page', ['minimizeMenu', 'mainMenu'])
   },
   methods: {
-    ...mapActions('user', [
-      'logout',
-    ])
+    ...mapActions('user', ['logout'])
   }
 }
 </script>
